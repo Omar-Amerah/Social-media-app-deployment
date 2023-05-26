168fd0d7094a75557d43e0cb9e0181a19ec7a76c
Omar Amerah <omaramerah2@gmail.com>
2023-05-26 15:09:27 +0100

    Test

--- src/components/utils/users/LoginUser.js
+++ src/components/utils/users/LoginUser.js
@@ -1,30 +1,34 @@
 export default async function LoginUser(username, password) {
-    try {
-      const response = await fetch(`https://testhostserversocial.onrender.com/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-        }),
-        credentials: "include", // Include cookies in the request
-      });
-  
-      const data = await response.json();
-      if (response.ok) {
-        document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        document.cookie = `user=${data}; path=/;`;
-        //alert("Logged in");
-        return true;
-      } else {
-        if (data.errors !== undefined) {
-          alert(data.errors[0].msg);
-          return (data.message).toString();
-        }
-        alert(data.message);
+  try {
+    // Clear the "user" cookie
+    document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+    const response = await fetch(`https://testhostserversocial.onrender.com/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+      credentials: "include", // Include cookies in the request
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      // Set the "user" cookie
+      const userCookie = { id: data.id, name: data.name };
+      const userCookieString = JSON.stringify(userCookie);
+      document.cookie = `user=${encodeURIComponent(userCookieString)}; path=/;`;
+      //alert("Logged in");
+      return true;
+    } else {
+      if (data.errors !== undefined) {
+        alert(data.errors[0].msg);
+        return data.message.toString();
       }
-    } catch (error) {
-      alert(error.message);
+      alert(data.message);
     }
+  } catch (error) {
+    alert(error.message);
   }
-  
+}
