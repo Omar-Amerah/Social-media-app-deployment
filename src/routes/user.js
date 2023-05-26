30dab1b656a3243a9ef8186551f7adbaad3af003
Omar Amerah <omaramerah2@gmail.com>
2023-05-26 13:08:27 +0100

    Changes

--- src/routes/user.js
+++ src/routes/user.js
@@ -69,7 +69,7 @@ router.get("/user/:id", async (req, res) => {
       const userCookie = { id: user.id, name: user.name };
       const userCookieString = JSON.stringify(userCookie);
       res.cookie("user", userCookieString);
-      
+      console.log("Logged user in")
       res.json({
         message: "Successfully logged in",
       });
