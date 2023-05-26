54cef1f32501ec1dd8bfc6e7403e819df5ab8790
Omar Amerah <omaramerah2@gmail.com>
2023-05-26 15:59:59 +0100

    Work

--- src/server.js
+++ src/server.js
@@ -2,22 +2,14 @@ const session = require('express-session');
 const express = require('express');
 const { postRouter, userRouter } = require('./routes/');
 const cookieParser = require('cookie-parser');
-const crypto = require('crypto');
+const cors = require('cors');
 
 const app = express();
-
-// Generate a random session secret
-const generateSessionSecret = () => {
-  return crypto.randomBytes(32).toString('hex');
-};
-
-// Set the session secret
-const SESSION_SECRET = process.env.SESSION_SECRET || generateSessionSecret();
 
 app.use(cookieParser());
 app.use(
   session({
-    secret: SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || 'Super Secret (change it)',
     resave: true,
     saveUninitialized: false,
     cookie: {
@@ -27,6 +19,11 @@ app.use(
     },
   })
 );
+
+app.use(cors({
+  origin: 'http://localhost:3000', // Replace with the origin of your frontend app
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use('/', postRouter, userRouter);
