e689454ebe3c8f60fdee05ac9e9cb428d19cda07
Omar Amerah <omaramerah2@gmail.com>
2023-05-30 14:00:36 +0100

    Config fix

---
+++ src/db/config.js
@@ -0,0 +1,10 @@
+const config = {
+    host: "testhostserversocial.onrender.com",
+    port: 5432,
+    database: "Social",
+    user: "Admin", 
+    password: "Password123*", 
+  };
+  
+  module.exports = config;
+  
